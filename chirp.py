51268a8a13c9525fb6fe85ecb5f8b1a24bb6c92e
William Deem <19997347+DeemOnSecurity@users.noreply.github.com>
2021-03-19 11:28:33 -0400

    Fixes #7, #8

--- chirp.py
+++ chirp.py
@@ -3,6 +3,8 @@
 # Standard Python Libraries
 from multiprocessing import freeze_support
 import os
+import sys
+import time
 
 # cisagov Libraries
 from chirp import run
@@ -12,13 +14,16 @@ if __name__ == "__main__":
     try:
         freeze_support()
         run.run()
+        time.sleep(2)
         CONSOLE(
-            "[green][+][/green] DONE! Your results can be found in {}.".format(
+            "[green][+][/green] DONE! Your results can be found in {}. Press any key to exit.".format(
                 os.path.abspath(OUTPUT_DIR)
             )
         )
+        input()
+        save_log()
+        sys.exit(0)
     except KeyboardInterrupt:
         ERROR("Received an escape sequence. Goodbye.")
-    finally:
         save_log()
-        input()
+        sys.exit(0)
--- chirp/__main__.py
+++ chirp/__main__.py
@@ -3,22 +3,27 @@
 # Standard Python Libraries
 from multiprocessing import freeze_support
 import os
+import sys
+import time
 
 # cisagov Libraries
+from chirp import run
 from chirp.common import CONSOLE, ERROR, OUTPUT_DIR, save_log
-import chirp.run
 
 if __name__ == "__main__":
     try:
         freeze_support()
-        chirp.run.run()
+        run.run()
+        time.sleep(2)
         CONSOLE(
-            "[green][+][/green] DONE! Your results can be found in {}.".format(
+            "[green][+][/green] DONE! Your results can be found in {}. Press any key to exit.".format(
                 os.path.abspath(OUTPUT_DIR)
             )
         )
+        input()
+        save_log()
+        sys.exit(0)
     except KeyboardInterrupt:
         ERROR("Received an escape sequence. Goodbye.")
-    finally:
         save_log()
-        input()
+        sys.exit(0)
